41bd7bfd8ff26f4952898dc8e69a11a0df72d83c
Maximandri <71848507+Maximandri@users.noreply.github.com>
2021-06-16 07:14:11 +0200

    Update SocketHandler.java

--- app/src/main/java/com/example/myapplication/SocketHandler.java
+++ app/src/main/java/com/example/myapplication/SocketHandler.java
@@ -1,13 +1,13 @@
 package com.example.myapplication;
 
-import android.os.AsyncTask;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,8 +15,8 @@ import java.util.concurrent.Executors;
 public class SocketHandler {
 
     private static Socket socket;
-    private PrintWriter pw;
-    private BufferedReader br;
+    private static PrintWriter pw;
+    private static BufferedReader br;
 
     private static int cores = Runtime.getRuntime().availableProcessors();
     private static ExecutorService executor = Executors.newFixedThreadPool(cores + 1);
@@ -29,7 +29,7 @@ public class SocketHandler {
         SocketHandler.socket = socket;
     }
 
-    public void sendString(String... voids){
+    public static synchronized void sendString(String... voids){
 
         String message = voids[0];
 
@@ -47,6 +47,36 @@ public class SocketHandler {
                 e.printStackTrace();
             }
         });
-
     }
+
+    public static synchronized List<String> receiveFolders(){
+        List<String> folders = new ArrayList<String>();
+
+        executor.execute(() -> {
+
+            try {
+                br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                System.out.println(br.ready());
+
+                String line = br.readLine();
+                System.out.println(line);
+
+                while (line != null && line.length() > 0) {
+                    folders.add(line);
+                    System.out.println(line);
+                    line = br.readLine();
+                }
+
+                br.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        return folders;
+    }
+
 }
